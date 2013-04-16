8dca8c2002c10ed084e3a3fbb405a93fd74d7a75
Dominiek ter Heide <info@dominiek.com>
2013-04-16 17:46:02 +0200

    Better error reporting

--- test/unit/utils.js
+++ test/unit/utils.js
@@ -16,11 +16,17 @@ var failureReport = {
     {
       "title":"Test B",
       "passed":false,
-      "error":{"name":"AssertionError","actual":100,"expected":90,"operator":"=="},
+      "error":{
+        "name":"AssertionError",
+        "actual":100,
+        "expected":90,
+        "operator":"==",
+        "stack": "AssertionError: 100 == 90\n    at Context.<anonymous> (/Users/dodo/checkouts/buoy/examples/my-project/test/my-test-fail.js:7:12)\n    at Test.Runnable.run (/Users/dodo/checkouts/buoy/node_modules/mocha/lib/runnable.js:213:32)\n    at Runner.runTest (/Users/dodo/checkouts/buoy/node_modules/mocha/lib/runner.js:351:10)\n    at Runner.runTests.next (/Users/dodo/checkouts/buoy/node_modules/mocha/lib/runner.js:397:12)\n    at next (/Users/dodo/checkouts/buoy/node_modules/mocha/lib/runner.js:277:14)\n    at Runner.hooks (/Users/dodo/checkouts/buoy/node_modules/mocha/lib/runner.js:286:7)\n    at next (/Users/dodo/checkouts/buoy/node_modules/mocha/lib/runner.js:234:23)\n    at Runner.hook (/Users/dodo/checkouts/buoy/node_modules/mocha/lib/runner.js:254:5)\n    at process.startup.processNextTick.process._tickCallback (node.js:244:9)"
+      },
       "duration":40,
       "timedOut":false,
       "namespace":["MyParent"]
-    }
+    },
   ],
   "numFailed": 1,
   "numPassed": 1,
@@ -40,6 +46,16 @@ describe("Utils", function() {
     failureReport.build.commitUrl = 'http://aaa'
     var summary = utils.summarizeReport(failureReport);
     assert(summary.match(/commit/i))
+
+  });
+
+
+  it("should generate error report", function() {
+
+    var errorSummary = utils.summarizeErrors(failureReport);
+    assert(errorSummary.match(/AssertionError/i))
+    assert(errorSummary.match(/MyParent/i))
+    assert(errorSummary.match(/checkouts/i))
 
   });
 
